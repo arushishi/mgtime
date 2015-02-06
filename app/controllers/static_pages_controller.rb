8f7ced1f7a5a8e943a6aa5e662e67885fdeaa139
e_katata <ekatata@miraholdings.co.jp>
2015-02-06 17:53:29 +0900

    v1-finish

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,35 @@ class StaticPagesController < ApplicationController
   def home
     @m_user = current_user
     if @m_user
-      @t_punchdata = @m_user.t_punchdatum.paginate(page: params[:page])
+   #   @t_punchdata = @m_user.t_punchdatum.paginate(page: params[:page])
+      @t_punchdata = ActiveRecord::Base.connection.select(<<SQL)
+select * from(
+SELECT
+    access_control_kubun as "access_control_kubun"
+  , punch_time as "punch_time"
+  , 'punch'  as "data_kubun"
+  , ''  as "comment"
+  , id as "t_punchdatum_id"
+FROM
+  dmgtime.t_punchdata 
+where m_user_id = 1
+and id not in (SELECT t_punchdatum_id FROM dmgtime.t_correcteddata where m_user_id = 1)
+union all
+SELECT
+    access_control_kubun as "access_control_kubun"
+  , corrected_time as "punch_time"
+  , 'corrected'  as "data_kubun"
+  , comment  as "comment"
+  , '-' as "t_punchdatum_id"
+FROM
+  dmgtime.t_correcteddata 
+where m_user_id = 1
+and corrected_time is not null
+) u
+order by u.punch_time desc, u.access_control_kubun, u.t_punchdatum_id desc
+;
+SQL
+ #.paginate(page: params[:page])
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,9 @@ Rails.application.routes.draw do
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
-  match '/punchdata/new', to: 't_punchdata#new', via: 'post'
+  match '/punchdata/new',  to: 't_punchdata#new',       via: 'post'
+  match '/punchdata/list', to: 't_correcteddata#index', via: 'post'
+  match '/punchdata/edit', to: 't_correcteddata#edit',  via: 'post'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
