d443c27fe674369ddb66b704407f49a9762db791
katata_home <ekatata@miraholdings.co.jp>
2015-02-07 00:10:53 +0900

    test

--- db/migrate/20150203190252_create_m_cards.rb
+++ db/migrate/20150203190252_create_m_cards.rb
@@ -1,10 +1,9 @@
 class CreateMCards < ActiveRecord::Migration
   def change
     create_table :m_cards do |t|
-      t.comment 'カードマスタ'
-      t.string :card_identification_code, :limit => 32, :null => false, :default => 'No ID CODE', :comment => 'カード識別コード'
-      t.string :m_user_id, :limit => 32, :null => false, :default => 'No User', :comment => 'ユーザID'
-      t.integer :delete_flg, :limit => 1, :null => false, :default => 0, :comment => '削除フラグ'
+      t.string :card_identification_code
+      t.string :m_user_id
+      t.integer :delete_flg
 
       t.timestamps
     end
--- db/migrate/20150205200251_create_t_correcteddata.rb
+++ db/migrate/20150205200251_create_t_correcteddata.rb
@@ -1,13 +1,12 @@
 class CreateTCorrecteddata < ActiveRecord::Migration
   def change
     create_table :t_correcteddata do |t|
-      t.comment '修正データ'
-      t.integer :t_punchdatum_id, :limit => 11, :null => false, :default => 0, :comment => '打刻データID'
-      t.integer :m_user_id, :limit => 11, :null => false, :default => 0, :comment => 'ユーザID'
-      t.string :access_control_kubun, :limit => 7, :null => false, :default => 'None', :comment => '入退室区分（Arrived：出勤、Left：退勤、GoOut：外出、GoBack：戻り）'
-      t.timestamp :corrected_time, :null => true, :comment => '日時'
-      t.string :comment, :limit => 128, :null => true, :comment => 'コメント'
-      t.integer :delete_flg, :limit => 1, :null => false, :default => 0, :comment => '削除フラグ'
+      t.integer :t_punchdatum_id
+      t.integer :m_user_id
+      t.string :access_control_kubun
+      t.timestamp :corrected_time
+      t.string :comment
+      t.integer :delete_flg
 
       t.timestamps
     end
